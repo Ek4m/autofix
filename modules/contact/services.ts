bac5b98bbfcd37cf833e8ef260251a6b306cdaea
Ek4m <elvin.james.99@gmail.com>
2026-05-31 14:28:49 +0400

    Contact migration

--- modules/contact/services.ts
+++ modules/contact/services.ts
@@ -4,7 +4,7 @@ import { ContactUsForm } from "./types/dtos";
 export const sendContactMessage = async (
   body: ContactUsForm,
 ): Promise<{ message: string }> => {
-  const response = await httpClient("/send-contact-message", {
+  const response = await httpClient("/contact/send", {
     body: JSON.stringify(body),
     method: "POST",
   });
